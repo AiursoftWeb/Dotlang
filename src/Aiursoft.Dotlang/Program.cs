9b18634164b2d25aca36687fffb8bbfc1cdcb013
Anduin Xue <xuef@microsoft.com>
2023-11-02 06:08:25 +0000

    Use latest command framework.

--- src/Aiursoft.Dotlang/Program.cs
+++ src/Aiursoft.Dotlang/Program.cs
@@ -1,15 +1,16 @@
 ﻿using Aiursoft.Dotlang.Core.Framework;
 using Aiursoft.Dotlang.BingTranslate;
-using System.CommandLine;
-using System.Reflection;
+using Aiursoft.CommandFramework;
 using Aiursoft.CommandFramework.Extensions;
 
-var descriptionAttribute = (Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly()).GetCustomAttribute<AssemblyDescriptionAttribute>()?.Description;
-
-var program = new RootCommand(descriptionAttribute ?? "Unknown usage.")
-    .AddGlobalOptions()
-    .AddPlugins(
-        new BingTranslatePlugin()
-    );
-
-return await program.InvokeAsync(args);
+return await new AiursoftCommand()
+    .Configure(command =>
+    {
+        command
+            .AddGlobalOptions()
+            .AddPlugins(
+                new BingTranslatePlugin()
+            );
+    })
+    .RunAsync(args);
+    
--- tests/Aiursoft.Dotlang.Tests/IntegrationTests.cs
+++ tests/Aiursoft.Dotlang.Tests/IntegrationTests.cs
@@ -1,4 +1,4 @@
-﻿using System.CommandLine;
+﻿using Aiursoft.CommandFramework;
 using Aiursoft.CommandFramework.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Aiursoft.Dotlang.Core.Framework;
@@ -9,49 +9,53 @@ namespace Dotlang.Tests;
 [TestClass]
 public class IntegrationTests
 {
-    private readonly RootCommand _program;
+    private readonly AiursoftCommand _program;
 
     public IntegrationTests()
     {
-        _program = new RootCommand("Test command.")
-            .AddGlobalOptions()
-            .AddPlugins(
-                new BingTranslatePlugin()
-            );
+        _program = new AiursoftCommand()
+            .Configure(command =>
+            {
+                command
+                    .AddGlobalOptions()
+                    .AddPlugins(
+                        new BingTranslatePlugin()
+                    );
+            });
     }
 
     [TestMethod]
     public async Task InvokeHelp()
     {
-        var result = await _program.InvokeAsync(new[] { "--help" });
-        Assert.AreEqual(0, result);
+        var result = await _program.TestRunAsync(new[] { "--help" });
+        Assert.AreEqual(0, result.ProgramReturn);
     }
 
     [TestMethod]
     public async Task InvokeVersion()
     {
-        var result = await _program.InvokeAsync(new[] { "--version" });
-        Assert.AreEqual(0, result);
+        var result = await _program.TestRunAsync(new[] { "--version" });
+        Assert.AreEqual(0, result.ProgramReturn);
     }
 
     [TestMethod]
     public async Task InvokeUnknown()
     {
-        var result = await _program.InvokeAsync(new[] { "--wtf" });
-        Assert.AreEqual(1, result);
+        var result = await _program.TestRunAsync(new[] { "--wtf" });
+        Assert.AreEqual(1, result.ProgramReturn);
     }
 
     [TestMethod]
     public async Task InvokeWithoutArg()
     {
-        var result = await _program.InvokeAsync(Array.Empty<string>());
-        Assert.AreEqual(1, result);
+        var result = await _program.TestRunAsync(Array.Empty<string>());
+        Assert.AreEqual(1, result.ProgramReturn);
     }
 
     [TestMethod]
     public async Task InvokeTranslateWithoutArg()
     {
-        var result = await _program.InvokeAsync(new[] { "translate" });
-        Assert.AreEqual(1, result);
+        var result = await _program.TestRunAsync(new[] { "translate" });
+        Assert.AreEqual(1, result.ProgramReturn);
     }
 }
